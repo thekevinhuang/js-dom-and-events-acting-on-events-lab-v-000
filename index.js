33985f3acecaeac8829b45338ca415cf7c9afed9
Kevin Huang <yellow.khaos@gmail.com>
2018-10-15 01:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi(){
+  document.querySelector('ul.employee-list').append(`<li> ${retrieveEmployeeInformation()} </li>`)
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type=submit]').addEventListener('click', function() {
+    addNewElementAsLi()
+    input.value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+    document.querySelector('a').addEventListener('click',function () {
+      let employeeList = document.querySelector('ul.employee-list')
+      while(employeeList.firstChild){
+        employeeList.removeChild(employeeList.firstChild)
+      }
+    })
+}
